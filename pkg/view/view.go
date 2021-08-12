9e7cfad76090d7bff4cd1c102b404cab791746d6
nishihong <249183418@qq.com>
2021-08-12 15:13:04 +0800

    【A】Flash 消息提示

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -2,6 +2,7 @@ package view
 
 import (
 	"ch35/goblog/pkg/auth"
+	"ch35/goblog/pkg/flash"
 	"ch35/goblog/pkg/logger"
 	"ch35/goblog/pkg/route"
 	"html/template"
@@ -28,6 +29,8 @@ func RenderTemplate(w io.Writer, name string, data D, tplFiles ...string) {
 
 	// 1. 通用模板数据
 	data["isLogined"] = auth.Check()
+	data["loginUser"] = auth.User
+	data["flash"] = flash.All()
 
 	// 2. 生成模板文件
 	allFiles := getTemplateFiles(tplFiles...)
