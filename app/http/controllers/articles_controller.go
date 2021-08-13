e3c1b1313488979524a8f6a7247af00ca67533ec
nishihong <249183418@qq.com>
2021-08-13 16:36:11 +0800

    【A】配置和环境变量

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -89,6 +89,9 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 
 // Index 文章列表页
 func (*ArticlesController) Index(w http.ResponseWriter, r *http.Request) {
+
+	//fmt.Fprint(w, config.Get("app.name"))
+
 	// 1. 获取结果集
 	articles, err := article.GetAll()
 	//fmt.Println(articles)
--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"ch35/goblog/app/http/middlewares"
 	"ch35/goblog/bootstrap"
+	"ch35/goblog/config"
+	c "ch35/goblog/pkg/config"
 	"net/http"
 )
+
+func init() {
+	// 初始化配置信息
+	config.Initialize()
+}
 
 func main() {
 	bootstrap.SetupDB()
 	router := bootstrap.SetupRoute()
 
-	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	//http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	http.ListenAndServe(":"+c.GetString("app.port"), middlewares.RemoveTrailingSlash(router))
 }
