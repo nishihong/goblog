d3eb37399a62cb26caca1600472a0f94a40d6fe1
nishihong <249183418@qq.com>
2021-08-17 10:48:20 +0800

    【A】教程结束 END

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -2,7 +2,12 @@ package article
 
 import (
 	"ch35/goblog/app/models"
+	"ch35/goblog/app/models/category"
 	"ch35/goblog/app/models/user"
+	"ch35/goblog/pkg/model"
+	"ch35/goblog/pkg/pagination"
+	"net/http"
+
 	//"ch35/goblog/pkg/logger"
 	"ch35/goblog/pkg/route"
 )
@@ -16,6 +21,9 @@ type Article struct {
 
 	UserID uint64 `gorm:"not null;index"`
 	User   user.User
+
+	CategoryID uint64 `gorm:"not null;default:4;index"`
+	Category   category.Category
 }
 
 // Link 方法用来生成文章链接
@@ -26,4 +34,21 @@ func (a Article) Link() string {
 // CreatedAtDate 创建日期
 func (a Article) CreatedAtDate() string {
 	return a.CreatedAt.Format("2006-01-02")
+}
+
+// GetByCategoryID 获取分类相关的文章
+func GetByCategoryID(cid string, r *http.Request, perPage int) ([]Article, pagination.ViewData, error) {
+
+	// 1. 初始化分页实例
+	db := model.DB.Model(Article{}).Where("category_id = ?", cid).Order("created_at desc")
+	_pager := pagination.New(r, db, route.Name2URL("categories.show", "id", cid), perPage)
+
+	// 2. 获取视图数据
+	viewData := _pager.Paging()
+
+	// 3. 获取数据
+	var articles []Article
+	_pager.Results(&articles)
+
+	return articles, viewData, nil
 }
--- app/models/category/category.go
+++ app/models/category/category.go
@@ -2,7 +2,9 @@ package category
 
 import (
 	"ch35/goblog/app/models"
+	"ch35/goblog/pkg/model"
 	"ch35/goblog/pkg/route"
+	"ch35/goblog/pkg/types"
 )
 
 // Category 文章分类
@@ -15,4 +17,15 @@ type Category struct {
 // Link 方法用来生成文章链接
 func (c Category) Link() string {
 	return route.Name2URL("categories.show", "id", c.GetStringID())
+}
+
+// Get 通过 ID 获取分类
+func Get(idstr string) (Category, error) {
+	var category Category
+	id := types.StringToInt(idstr)
+	if err := model.DB.First(&category, id).Error; err != nil {
+		return category, err
+	}
+
+	return category, nil
 }
